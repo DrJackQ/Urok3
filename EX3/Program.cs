6d6cdaa2a832ad4d8ab8c084249080c4e16508c2
Evgeny <drjackq@yandex.ru>
2023-02-27 15:48:06 +0300

    Добавили EX3

---
+++ EX3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вид 21
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++; // Инкримент - это увелечение счетчика на 1. 
+                 // Декримент - это уменьшение счетчика на 1.
+    }
+}
+Method21("Текст", 4);
