2e05a3be4c8499d7813560078f2dd5dcd0a747a2
Evgeny <drjackq@yandex.ru>
2023-03-01 13:17:08 +0300

    Добавили EX9

---
+++ EX9/Program.cs
@@ -0,0 +1,36 @@
+﻿int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1 };
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for(int i = 0; i < count; i++)
+    {
+        Console.WriteLine($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+
+void Selec(int[] array)
+{
+     for(int i = 0; i < array.Length - 1; i++)
+     {
+        int Pos = 1;
+
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[Pos]) Pos = j;
+        }
+        //
+        //
+        //
+        int temparray = array[i];
+        array[i] = array[Pos];
+        array[Pos] = temparray;
+     }
+}
+
+PrintArray(arr);
+Selec(arr);
+
+
+PrintArray(arr);
